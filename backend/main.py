a624cd3912c6520720206020c074ba2a8a42ca8d
Dipu Sharma <sdipu231@gmail.com>
2026-03-02 19:47:00 +0530

    file name changed

--- backend/main.py
+++ backend/main.py
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8002))
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
