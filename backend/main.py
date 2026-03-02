b31e1070a2f5e01d7dc5ff3718cdfcca1703cd3c
Dipu Sharma <sdipu231@gmail.com>
2026-03-02 19:58:58 +0530

    ulr add

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,10 @@ app = FastAPI(title="HRMS Lite API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://test-assisment-98ykor7my-dipu-sharmas-projects.vercel.app",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
